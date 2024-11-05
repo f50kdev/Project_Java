f30dadb8d860a1c7d2e79973e5bcb19691309db9
f50kdev <eadpea2020@gmail.com>
2024-11-05 09:28:21 +0530

    Variable_in_java

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ola World");
+
+        int ageOfFather  =  23;
+        System.out.println("the age is " + ageOfFather);
+
+        //PRIMIVE TYPES
+        int age ;
+        double  salary;
+        float pointflow;
+        boolean truthTable =  true;
+        boolean falseTable = false;
+        char characteres  = 'a';
+
+
+
+
+    }
+}
---
+++ src/operadoredores.java
@@ -0,0 +1,69 @@
+import javax.swing.text.TabableView;
+import javax.swing.text.TableView;
+
+public class operadoredores {
+    public static void main(String[] args) {
+        int number = 12;
+        double numberOne =10;
+        double NuumberTwo = 22;
+
+        //the resul of int valuem always will be int value;
+
+        double soma = (numberOne /  (double) NuumberTwo);
+
+        System.out.println("the sum of Number" + numberOne + " and Number"+ NuumberTwo +  "Is equal = " + (soma));
+        //tipos primitovs
+
+        int Date  = 13;
+
+        int Idade ;
+        System.out.println(Date);
+        System.out.println("a Idade e" + Date );
+
+        // boolean value
+         boolean falson = false;
+         Boolean verdadeiro = true;
+         long Highernumber = 192938;
+         byte idade = 12;
+         char tableVlue = 65;
+
+        System.out.println(idade + "+" + falson);
+        char ValoresTabbela_ASCI =  01;
+        System.out.println(ValoresTabbela_ASCI);
+        System.out.println(tableVlue);
+
+
+
+        // typecasting
+        int casting =92;
+        double   castings  = 12.23D;
+
+         int resto = 20%2;
+
+         //booleanos
+
+         boolean isDezMaiorQueVinte = 10<20;
+            boolean isTeemLessThanFive  = 10 < 5;
+        //aplicando operadoresRelacionais
+
+        // aboutSalary
+
+    int age =   29;
+    float salary  =3561;
+
+     boolean isIntOfflaw  =  age < 30 && salary <2500;
+     System.out.println(isIntOfflaw);
+
+     //Logic operator
+
+        double valorTotalContaCorrente  = 200;
+        double valorTotalContaPoupanca  = 10000;
+        float PriceOfPlaystation  = 5000F;
+        boolean isPlaystationFiveBuyOrNot = valorTotalContaCorrente >
+
+
+
+
+
+    }
+}

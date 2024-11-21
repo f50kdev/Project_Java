20040ec178f4f3528c90912db489edd671cb87a1
f50kdev <eadpea2020@gmail.com>
2024-11-21 06:13:30 +0530

    inhiritance

---
+++ src/encapsuation.java
@@ -0,0 +1,22 @@
+class homem{
+    private  String nome;
+    private int age ;
+
+    public void setAge(int ages) {
+         age  = ages;
+    }
+
+    public void  setNome(String Name){
+        nome = Name;
+    }
+}
+
+public class encapsuation {
+    public static void main(String[] args) {
+        homem objecto = new homem();
+        objecto.setNome("Whitney");
+
+
+
+    }
+}
---
+++ src/operadoresTernarios.java
@@ -0,0 +1,12 @@
+public class operadoresTernarios {
+    public static void main(String[] args) {
+        //Doar salalrio >500
+        double salario = 300;
+
+        String mensagemDoar = "Eu vou doar 500 pro Faustino";
+        String mensagemNaoDoar = "Ainda nao tenho como doar";
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+
+    }
+}

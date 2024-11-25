202a46ecbfce407a6ce578e2c56e8469b0e6c40f
f50kdev <eadpea2020@gmail.com>
2024-11-26 01:43:36 +0530

    Table truth variabele

--- src/TrutTable.java
+++ src/TrutTable.java
@@ -1,2 +1,14 @@
 public class TrutTable {
+    public static void main(String[] args) {
+        //salary normal
+        //primeira parceladadataxa
+        // segunda faixa
+        //segunda faixa
+        //impostos
+
+        double SalaryByyear = 70000;
+        double firstfee;
+        double secondfee;
+        double impostos;
+    }
 }

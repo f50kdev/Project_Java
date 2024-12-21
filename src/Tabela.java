be33995d218ce82408f68a16f09f13718f36d849
f50kdev <eadpea2020@gmail.com>
2024-12-21 12:53:45 +0530

    fees

---
+++ src/Tabela.java
@@ -0,0 +1,21 @@
+import  java.io.IOException;
+import java.io.BufferedReader;
+
+public class Tabela {
+    public static  void  main(String[] args) throws IOException{
+        System.out.println("hello");
+        float salary , fee , fee2 , fee3  , descount , total;
+        fee  = 100;
+
+        fee3 = 200;
+        salary = 10000;
+        System.out.printf("insert any value");
+         int capture  = System.in.read();
+
+            float salaryofyear ;
+        System.out.println(capture);
+
+        salaryofyear = salary * 12 ;
+
+    }
+}
